ab8273837d34b2cdc217022d9bd34cff4326bc80
Andrew Carpenter <andrew@audioinhd.com>
2017-01-24 20:51:17 -0500

    Make id option, add a couple tests, needs more

--- test/xml2js_test.js
+++ test/xml2js_test.js
@@ -5,184 +5,184 @@ var testItems = require('./test-items');
 
 describe('Testing xml2js.js:', function () {
     'use strict';
-    
+
     //var books = require('fs').readFileSync('test/fixtures/books.xml');
-    
+
     /*describe('No options supplied (fallback to defaults):', function () {
-        
+
         var options = {};
         testItems(options).forEach(function (test) {
             it(test.desc, function () {
                 expect(convert.xml2js(test.xml, options)).toEqual(test.js);
             });
         });
-        
+
     });*/
-    
+
     describe('No options supplied (fallback to defaults):', function () {
-        
+
         var options = {};
         testItems(options).forEach(function (test) {
             it(test.desc, function () {
                 expect(convert.xml2js(test.xml, options)).toEqual(test.js);
             });
         });
-        
+
     });
-    
+
     describe('options = {compact: false}', function () {
-        
+
         describe('Options set to default values explicitly:', function () {
-            
+
             var options = {singleLine: false, compact: false, trim: false, sanitize: false, nativeType: false, alwaysChildren: false, addParent: false};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: false}', function () {
-            
+
             var options = {compact: false};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: false, trim: true}', function () {
-            
+
             var options = {compact: false, trim: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: false, sanitize: true}', function () {
-            
+
             var options = {compact: false, sanitize: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: false, nativeType: true}', function () {
-            
+
             var options = {compact: false, nativeType: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: false, alwaysChildren: true}', function () {
-            
+
             var options = {compact: false, alwaysChildren: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: false, addParent: true}', function () {
-            
+
             var options = {compact: false, addParent: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
     });
-    
+
     describe('options = {compact: true}', function () {
-        
+
         describe('Options set to default values explicitly:', function () {
-            
+
             var options = {compact: true, trim: false, sanitize: false, nativeType: false, alwaysChildren: false, addParent: false};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: true}', function () {
-            
+
             var options = {compact: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: true, trim: true}', function () {
-            
+
             var options = {compact: true, trim: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: true, sanitize: true}', function () {
-            
+
             var options = {compact: true, sanitize: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: true, alwaysChildren: true}', function () {
-            
+
             var options = {compact: true, alwaysChildren: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: true, addParent: true}', function () {
-            
+
             var options = {compact: true, addParent: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2js(test.xml, options)).toEqual(test.js);
                 });
             });
-            
+
         });
-        
+
     });
-    
+
     describe('Various options:', function () {
-        
+
         describe('options = {trim: true}', function () {
 
             var options = {trim: true};
@@ -193,7 +193,25 @@ describe('Testing xml2js.js:', function () {
             });
 
         });
-        
+
+        describe('options = {compact: true, addId: true}', function () {
+
+            var options = {compact: true, addId: true};
+            it('Has id', function () {
+                expect(convert.xml2js('<a>123</a>', options).a._id).toBeTruthy();
+            });
+
+        });
+
+        describe('options = compact: true, addId: true, idKey: "xId"', function () {
+
+            var options = {compact: true, addId: true, idKey: 'xId'};
+            it('Has xId', function () {
+                expect(convert.xml2js('<a>123</a>', options).a.xId).toBeTruthy();
+            });
+
+        });
+
         describe('options = {nativeType: true}', function () {
 
             var options = {nativeType: true};
@@ -213,37 +231,37 @@ describe('Testing xml2js.js:', function () {
             });*/
 
         });
-        
+
     });
-    
+
     describe('xml2json:', function () {
-        
+
         describe('No options supplied (fallback to defaults):', function () {
-            
+
             var options = {};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2json(test.xml, options)).toEqual(JSON.stringify(test.js));
                 });
             });
-            
+
         });
-        
+
         describe('options = {compact: true, addParent: true}:', function () {
-            
+
             var options = {onlyItem: 6, compact: true, addParent: true};
             testItems(options).forEach(function (test) {
                 it(test.desc, function () {
                     expect(convert.xml2json(test.xml, options)).toBe(JSON.stringify(test.js, function (k, v) { return k === '_parent'? '_' : v; }));
                 });
             });
-            
+
         });
-        
+
     });
-    
+
     describe('User reported issues:', function () {
-        
+
         describe('case by Mark Pareja', function () {
 
             var xml = '<?xml version="1.0" encoding="utf-8"?>' + '\n' +
@@ -295,13 +313,13 @@ describe('Testing xml2js.js:', function () {
                     }
                 }
             };
-            
+
             it('should output as expected json', function () {
                 expect(convert.xml2json(xml, {compact: true})).toEqual(JSON.stringify(json));
             });
 
         });
-        
+
         describe('case by Félix Dion Robidoux', function () {
 
             var xml = '<ZohoCreator>' + '\n' +
@@ -329,15 +347,15 @@ describe('Testing xml2js.js:', function () {
                       '        </application>' + '\n' +
                       '    </applicationslist>' + '\n' +
                       '</ZohoCreator>';
-        
+
             var json = convert.xml2json(xml, {compact: true, spaces: 4});
-            
+
             it('should output json and reverse it back to xml', function () {
                 expect(convert.json2xml(json, {compact: true, spaces: 4, fullTagEmptyElement: true})).toEqual(xml);
             });
-            
+
         });
 
     });
-    
+
 });
